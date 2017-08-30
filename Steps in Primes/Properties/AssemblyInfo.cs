4c5ce8449fcf39fa9b6844e6e509fd1207f5a7c7
dlgs5100 <dlgs5100@gmail.com>
2017-08-30 12:39:57 +0800

    modify

---
+++ Steps in Primes/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Steps in Primes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Steps in Primes")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a63abb58-43ef-4300-8d47-165b7eabda6c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Steps in Primes/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Steps_in_Primes
+{
+
+    [TestFixture]
+    public static class StepInPrimesTests
+    {
+        [Test]
+        public static void test()
+        {
+            Assert.AreEqual(new int[] { 101, 103 }, StepInPrimes.Step(2, 100, 110));
+            Assert.AreEqual(new int[] { 103, 107 }, StepInPrimes.Step(4, 100, 110));
+            Assert.AreEqual(new int[] { 101, 107 }, StepInPrimes.Step(6, 100, 110));
+            Assert.AreEqual(new int[] { 359, 367 }, StepInPrimes.Step(8, 300, 400));
+            Assert.AreEqual(new int[] { 307, 317 }, StepInPrimes.Step(10, 300, 400));
+        }
+    }
+    class StepInPrimes
+    {
+        public static long[] Step(int g, long m, long n)
+        {
+            for (long i = m; i <= n; i++)
+            {
+                if (isPrime(i))
+                    if (isPrime(i + g) && (i + g) <= n)
+                        return new long[] { i, i + g };
+            }
+            return null;
+        }
+        public static bool isPrime(long value)
+        {
+            if (value == 1)
+                return false;
+            if (value % 2 == 0)
+            {
+                if (value == 2)
+                    return true;
+                else
+                    return false;
+            }
+
+            bool result = true;
+            for (long i = 3; (i * i) <= value; i += 2)
+            {
+                if (value % i == 0)
+                    result = false;
+            }
+            return result;
+        }
+    }
+}
